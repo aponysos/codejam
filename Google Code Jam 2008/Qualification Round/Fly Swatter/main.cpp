5a0fbe162a4f9b072a9761c9127559acfaefcc48
aponysos <aponysos@163.com>
2018-10-30 17:04:16 +0800

    [Fly Swatter] refracor and comment

--- Google Code Jam 2008/Qualification Round/Fly Swatter/main.cpp
+++ Google Code Jam 2008/Qualification Round/Fly Swatter/main.cpp
@@ -51,19 +51,14 @@ private:
   static double Nonnegative(double d);
   static double SectorArea(double radius, double theta);
   static double ChordArea(double radius, double theta);
-  static Point HorizontalCrossoverPoint(double radius, double y);
-  static Point VerticalCrossoverPoint(double radius, double x);
+  static double ModulusOf(double x, double y);
+  static Point  HorizontalCrossoverPoint(double radius, double y);
+  static Point  VerticalCrossoverPoint(double radius, double x);
 
 private:
   // add case-related members here
   double rFly_, rOuter_, thick_, rString_, gap_;
   double probability_;
-
-  // intermediate variables
-  double aUnhitting_; // area of unhitting area
-  double rUnhitting_; // radius of unhitting area
-  double slLattice_; // semi-length of a side of a single lattice
-  double aLattice_; // area of a single lattice
 };
 
 int main(int argc, char **argv)
@@ -83,73 +78,84 @@ void Case::Compute()
 {
   probability_ = 0.0; // init result
 
-  // init intermediate variables
-  aUnhitting_ = 0.0;
-  rUnhitting_ = Nonnegative(rOuter_ - thick_ - rFly_);
-  slLattice_  = Nonnegative(gap_ / 2 - rFly_);
-  aLattice_   = slLattice_ * slLattice_ * 4;
+  // intermediate variables
+  double rUnhitting = Nonnegative(rOuter_ - thick_ - rFly_); // R - t - f, radius of unhitting area
+  double slLattice  = Nonnegative(gap_ / 2 - rFly_);         // g/2 - f, semi-length of a side of a single lattice
+  double slGap      = rString_ + gap_ / 2;                   // r + g/2, semi-gap between lattices
 
-  bool isDone = false;
-  for (int i = 0; !isDone; ++i) {
-    for (int j = 0; ; ++j) {
-      Point pCenter(
-        rString_ + gap_ / 2 + i * (rString_ * 2 + gap_), 
-        rString_ + gap_ / 2 + j * (rString_ * 2 + gap_)); // center point of lattic (i, j)
-      Point pLT(pCenter + Point(-slLattice_,  slLattice_)); // left top point
-      Point pLB(pCenter + Point(-slLattice_, -slLattice_)); // left bottom point
-      Point pRT(pCenter + Point( slLattice_,  slLattice_)); // right top point
-      Point pRB(pCenter + Point( slLattice_, -slLattice_)); // right bottom point
+  double aLattice   = slLattice * slLattice * 4;  // area of a single lattice
+  double aUnhitting = 0.0;                        // area of unhitting area
 
-      // modulus of points
-      double mLT = abs(pLT);
-      double mLB = abs(pLB);
-      double mRT = abs(pRT);
-      double mRB = abs(pRB);
+  for (int i = 0; ; ++i) {
+    double xCenter = slGap * (2 * i + 1); // x coordinate of certer point
+    double xLeft   = xCenter - slLattice; // x coordinate of left edge
+    double xRight  = xCenter + slLattice; // x coordinate of right edge
 
-      if (mRT <= rUnhitting_) { // interior lattice
-        aUnhitting_ += aLattice_;
-        continue;
-      }
+    // get the top lattice(i, jLB) with its left-bottom point inside the sector
+    int jLB = -1; // -1 means none
+    double sdLB = rUnhitting * rUnhitting - xLeft * xLeft; // square difference
+    if (sdLB > 0)
+      jLB = static_cast<int>(floor(((sqrt(sdLB) + slLattice) / slGap) - 1) / 2);
 
-      if (mLB >= rUnhitting_) { // exterior lattice
-        if (j == 0) // no more lattices
-          isDone = true;
-        break;
-      }
+    if (jLB < 0) // no more lattices
+      break;
 
-      // edge lattice
-      Point pTop    = HorizontalCrossoverPoint(rUnhitting_, pLT.imag()); // top crossover point
-      Point pBottom = HorizontalCrossoverPoint(rUnhitting_, pRB.imag()); // bottom crossover point
-      Point pLeft   = VerticalCrossoverPoint(rUnhitting_, pLB.real()); // left crossover point
-      Point pRight  = VerticalCrossoverPoint(rUnhitting_, pRT.real()); // right crossover point
+    // get the top lattice(i, jRT) with its right-top point inside the sector
+    int jRT = -1; // -1 means none
+    double sdRT = rUnhitting * rUnhitting - xRight * xRight; // square difference
+    if (sdRT > 0)
+      jRT = static_cast<int>(floor(((sqrt(sdRT) - slLattice) / slGap) - 1) / 2);
 
-      double theta = 0.0; // angle of chord
+    // interior complete lattices
+    aUnhitting += aLattice * (jRT + 1);
+
+    // edge incomplete lattices
+    for (int j = jRT + 1; j <= jLB; ++j) {
+      double yCenter = slGap * (j * 2 + 1); // y coordinate of certer point
+      double yTop    = yCenter + slLattice; // y coordinate of top edge
+      double yBottom = yCenter - slLattice; // y coordinate of bottom edge
+
+      // modulus of vertices
+      double mLB = ModulusOf(xLeft,  yBottom); // left bottom point
+      double mLT = ModulusOf(xLeft,  yTop);    // left bottom point
+      double mRT = ModulusOf(xRight, yTop);    // right top point
+      double mRB = ModulusOf(xRight, yBottom); // right bottom point
+
+      // crossover points
+      Point pTop    = HorizontalCrossoverPoint(rUnhitting, yTop);     // top crossover point
+      Point pBottom = HorizontalCrossoverPoint(rUnhitting, yBottom);  // bottom crossover point
+      Point pLeft   = VerticalCrossoverPoint  (rUnhitting, xLeft);    // left crossover point
+      Point pRight  = VerticalCrossoverPoint  (rUnhitting, xRight);   // right crossover point
+
+      double theta    = 0.0; // angle of chord
       double aPolygon = 0.0; // area of polygon
 
-      if (mLT >= rUnhitting_ && mRB >= rUnhitting_) { // triangle
+      if (mLT >= rUnhitting && mRB >= rUnhitting) { // triangle
         theta = arg(pLeft) - arg(pBottom);
-        aPolygon = (pLeft.imag() - pLB.imag()) * (pBottom.real() - pLB.real()) / 2;
+        aPolygon = (pLeft.imag() - yBottom) * (pBottom.real() - xLeft) / 2;
       }
-      else if (mLT <= rUnhitting_ && mRB <= rUnhitting_) { // pentagon
+      else if (mLT <= rUnhitting && mRB <= rUnhitting) { // pentagon
         theta = arg(pTop) - arg(pRight);
-        aPolygon = aLattice_ - (pRT.imag() - pRight.imag()) * (pRT.real() - pTop.real()) / 2;
+        aPolygon = aLattice - (yTop - pRight.imag()) * (xRight - pTop.real()) / 2;
       }
-      else if (mLT >= rUnhitting_ && mRB <= rUnhitting_) { // trapezoid
+      else if (mLT >= rUnhitting && mRB <= rUnhitting) { // trapezoid
         theta = arg(pLeft) - arg(pRight);
-        aPolygon = ((pLeft.imag() - pLB.imag()) + (pRight.imag() - pRB.imag())) * slLattice_;
+        aPolygon = ((pLeft.imag() - yBottom) + (pRight.imag() - yBottom)) * slLattice;
       }
-      else if (mLT <= rUnhitting_ && mRB >= rUnhitting_) { // trapezoid
+      else if (mLT <= rUnhitting && mRB >= rUnhitting) { // trapezoid
         theta = arg(pTop) - arg(pBottom);
-        aPolygon = ((pTop.real() - pLT.real()) + (pBottom.real() - pLB.real())) * slLattice_;
+        aPolygon = ((pTop.real() - xLeft) + (pBottom.real() - xLeft)) * slLattice;
       }
+      else // should not get here
+        continue;
 
-      double aChord = ChordArea(rUnhitting_, theta); // area of chord
-      aUnhitting_ += (aPolygon + aChord); // acumulate area of chord and polygon
-    }
-  }
+      double aChord = ChordArea(rUnhitting, theta); // area of chord
+      aUnhitting += (aPolygon + aChord); // acumulate area of chord and polygon
+    } // for j
+  } // for i
 
   // hitting probability + unhitting probability == 1
-  probability_ = 1 - aUnhitting_ / SectorArea(rOuter_, SPI);
+  probability_ = 1 - aUnhitting / SectorArea(rOuter_, SPI);
 }
 
 void Case::WriteTo(std::ostream & os) const
@@ -173,6 +179,12 @@ double Case::SectorArea(double radius, double theta)
 double Case::ChordArea(double radius, double theta)
 {
   return SectorArea(radius, theta) - radius * radius * sin(theta / 2) * cos(theta / 2);
+}
+
+//static 
+double Case::ModulusOf(double x, double y)
+{
+  return abs(Point(x, y));
 }
 
 //static 
