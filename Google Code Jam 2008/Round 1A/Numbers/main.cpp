8c2ef33cb9459893c6265063bc4d217a337f4d79
aponysos <aponysos@163.com>
2018-11-13 18:00:22 +0800

    [Numbers] small output correct; large output incorrect

--- Google Code Jam 2008/Round 1A/Numbers/main.cpp
+++ Google Code Jam 2008/Round 1A/Numbers/main.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -41,12 +43,25 @@ public:
   void WriteTo(std::ostream & os) const; // write result to output stream
 
 private:
+  static int Mod125(Integer n); // n mod 125
+  static int Power3Mod125(Integer n); // (3 ^ n) mod 125
+  static vector<int> cachePower3Mod125_;
+  static int Mod1000FromMod125(int mod125); // get mod1000 from mod125
+  static vector<int> cacheMod1000FromMod125_;
+
+private:
   // add case-related members here
   Integer n_;
 
   // results
-  Integer modt_;
+  int mod1000_; // modulus of one thousand
 };
+
+//static 
+vector<int> Case::cachePower3Mod125_;
+
+//static 
+vector<int> Case::cacheMod1000FromMod125_;
 
 int main(int argc, char **argv)
 {
@@ -60,15 +75,66 @@ void Case::ReadFrom(std::istream & is)
 {
   is >> n_;
 
+  cachePower3Mod125_.resize(125, -1);
+  cachePower3Mod125_[0] = 1;
+
+  cacheMod1000FromMod125_.resize(125, -1);
+  for (int i = 0; i < 1000; i += 8)
+    cacheMod1000FromMod125_[i % 125] = i % 1000;
+
   // init results
-  modt_ = 0;
+  mod1000_ = 0;
 }
 
 void Case::Compute()
 {
+  if (n_ <= 4) {
+    long double base = 3 + sqrt(5);
+    mod1000_ = static_cast<int>(floor(pow(base, n_)));
+    mod1000_ %= 1000;
+  }
+  else {
+    int nmod125  = Mod125(n_);
+    int n1mod125 = Mod125(n_ - 1);
+    int n2mod125 = Mod125(n_ - 2);
+    int n3mod125 = Mod125(n_ - 3);
+    int power3nmod125  = Power3Mod125(n_);
+    int power3n2mod125 = Power3Mod125(n_ - 2);
+    int power3n4mod125 = Power3Mod125(n_ - 4);
+
+    int xnmod125 = 0; // xn mod 125
+    xnmod125 += Mod125(power3nmod125);
+    xnmod125 += Mod125(nmod125 * n1mod125 / 2 * power3n2mod125 * 5);
+    xnmod125 += Mod125(nmod125 * n1mod125 * n2mod125 * n3mod125 / 4 / 3 / 2 * power3n4mod125 * 25);
+    xnmod125  = Mod125(xnmod125 * 2);
+
+    mod1000_ = Mod1000FromMod125(xnmod125) - 1;
+    mod1000_ = mod1000_ < 0 ? 999 : mod1000_; // check -1
+  }
 }
 
 void Case::WriteTo(std::ostream & os) const
 {
-  os << modt_;
+  os << setw(3) << setfill('0') << mod1000_;
+}
+
+int Case::Mod125(Integer n)
+{
+  return n % 125;
+}
+
+int Case::Power3Mod125(Integer n)
+{
+  n = n % 124;
+
+  int & cache = cachePower3Mod125_[n];
+  if (cache >= 0)
+    return cache;
+  else
+    return cache = Mod125(Power3Mod125(n - 1) * 3);
+}
+
+int Case::Mod1000FromMod125(int mod125)
+{
+  return cacheMod1000FromMod125_[mod125];
 }
