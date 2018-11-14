0bd0ec7b644ab31d0883abdcf58f8c863b9475d0
aponysos <aponysos@163.com>
2018-11-14 10:16:02 +0800

    [Round 1B 2008] setup

---
+++ Google Code Jam 2008/Round 1B/Crop Triangles/main.cpp
@@ -0,0 +1,64 @@
+// Round 1B 2008
+// Problem A. Crop Triangles
+// https://code.google.com/codejam/contest/32017/dashboard#s=p0
+// Command Arguments : < $(Configuration).in > $(Configuration).out
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename TCase>
+class Problem
+{
+public:
+  void Solve(std::istream & is, std::ostream & os)
+  {
+    int nCase = 0;
+    is >> nCase;
+    cases_.resize(nCase);
+    for (int i = 0; i < nCase; ++i) {
+      TCase & c = cases_[i];
+      c.ReadFrom(is);
+      c.Compute();
+      os << "Case #" << i + 1 << ": ";
+      c.WriteTo(os);
+      os << '\n';
+    }
+  }
+
+private:
+  std::vector<TCase> cases_;
+};
+
+class Case
+{
+public:
+  void ReadFrom(std::istream & is); // read from input stream
+  void Compute(); // main body of alogrithm
+  void WriteTo(std::ostream & os) const; // write result to output stream
+
+private:
+  // add case-related members here
+};
+
+int main(int argc, char **argv)
+{
+  Problem<Case> p;
+  p.Solve(cin, cout);
+
+  return 0;
+}
+
+void Case::ReadFrom(std::istream & is)
+{
+}
+
+void Case::Compute()
+{
+}
+
+void Case::WriteTo(std::ostream & os) const
+{
+}
