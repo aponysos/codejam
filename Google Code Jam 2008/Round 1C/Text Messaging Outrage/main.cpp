82da6dd90ec2c79b9eeae12ac8a68105ccc3b585
aponysos <aponysos@163.com>
2020-01-26 23:20:08 +0800

    setup: Round 1C 2008

---
+++ Google Code Jam 2008/Round 1C/Text Messaging Outrage/main.cpp
@@ -0,0 +1,90 @@
+// Round
+// Problem
+// URL
+// Command Arguments : < $(Configuration).in > $(Configuration).out
+//
+
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+template <typename TCase>
+class Problem
+{
+public:
+  void Solve(std::istream &is, std::ostream &os)
+  {
+    int nCase = 0;
+    is >> nCase;
+    for (int i = 0; i < nCase; ++i)
+    {
+      TCase c(i + 1);
+      c.ReadFrom(is);
+      c.Compute();
+      os << "Case #" << i + 1 << ": ";
+      c.WriteTo(os);
+      os << '\n';
+    }
+  }
+};
+
+class Case
+{
+public:
+  Case(int iCase) : iCase_(iCase) {}
+
+public:
+  void ReadFrom(std::istream &is);      // read from input stream
+  void WriteTo(std::ostream &os) const; // write result to output stream
+
+  void Compute(); // main body of alogrithm
+
+private:
+  int iCase_; // case # iCase_, 1-based
+
+  // input & output members
+
+  // intermediate members
+};
+
+// auto-output execution time of a function
+class AutoClock
+{
+public:
+  AutoClock(const std::string &funcName) : funcName_(funcName), start_(std::chrono::system_clock::now()) {}
+  ~AutoClock()
+  {
+    auto elapsed_ = std::chrono::system_clock::now() - start_;
+    std::clog << funcName_ << ": " << (double)elapsed_.count() / 1000 / 1000 << "ms" << std::endl;
+  }
+
+private:
+  std::string funcName_;
+  std::chrono::time_point<std::chrono::system_clock> start_;
+};
+
+#define TRACE() AutoClock ac(__FUNCTION__)
+//#define TRACE()
+
+int main(int argc, char **argv)
+{
+  Problem<Case> p;
+  p.Solve(cin, cout);
+
+  return 0;
+}
+
+void Case::ReadFrom(std::istream &is)
+{
+}
+
+void Case::WriteTo(std::ostream &os) const
+{
+}
+
+void Case::Compute()
+{
+  TRACE();
+  clog << "Case #" << iCase_ << ": " << '\n';
+}
